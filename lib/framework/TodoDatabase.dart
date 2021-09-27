d60464c568bf79115479c28977675b8389eafad6
shashankdaima <daimashashank10@gmail.com>
2021-09-28 04:08:10 +0530

    Added database interface

---
+++ lib/framework/TodoDatabase.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_todo/data/Todo.dart';
+
+class TodoDatabase{
+  void addItems(List<Todo> list){}
+  void addItem(Todo todo){}
+  void removeItem(int id){}
+  void getAllItems(){}
+
+}
